667a3aa4b813122aff0e22df260b6240d0150830
Guntas007 <149225595+Guntas007@users.noreply.github.com>
2024-06-04 19:04:42 -0400

    Create BlueHomeBase.java

---
+++ BlueHomeBase.java
@@ -0,0 +1,33 @@
+package View;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class BlueHomeBase {
+    private static final Color BLUE_COLOR = new Color(34, 64, 154);
+
+    public void draw(Graphics g, int cellSize) {
+        g.setColor(BLUE_COLOR);
+        g.fillRect(9 * cellSize, 9 * cellSize, 6 * cellSize, 6 * cellSize);
+        g.fillRect(13 * cellSize, 8 * cellSize, cellSize, cellSize);
+        g.fillRect(9 * cellSize, 7 * cellSize, 5 * cellSize, cellSize);
+        g.fillRect(8 * cellSize, 12 * cellSize, cellSize, cellSize);
+
+        drawWhiteGrid(g, cellSize, 11, 11);
+    }
+
+    private void drawWhiteGrid(Graphics g, int cellSize, int offsetX, int offsetY) {
+        g.setColor(Color.WHITE);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                g.fillRect((offsetX + i) * cellSize, (offsetY + j) * cellSize, cellSize, cellSize);
+            }
+        }
+
+        g.setColor(Color.BLACK);
+        for (int i = 0; i <= 2; i++) {
+            g.drawLine((offsetX + i) * cellSize, offsetY * cellSize, (offsetX + i) * cellSize, (offsetY + 2) * cellSize);
+            g.drawLine(offsetX * cellSize, (offsetY + i) * cellSize, (offsetX + 2) * cellSize, (offsetY + i) * cellSize);
+        }
+    }
+}
