8547abc69cf24b81d2260b6dc8bbc96fa24d6c4c
Guntas007 <149225595+Guntas007@users.noreply.github.com>
2024-07-25 15:53:16 -0400

    Update ChatPanel.java
    added a chat heading

--- ChatPanel.java
+++ ChatPanel.java
@@ -5,25 +5,35 @@ import java.awt.*;
 
 public class ChatPanel extends JPanel {
     private static final long serialVersionUID = 1L;
+    private JLabel chatHeadLabel;
 
     public ChatPanel() {
         setLayout(new BorderLayout());
         setPreferredSize(new Dimension(400, 700));
 
+        // Create JPanel for top portion with FlowLayout
+     	JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+     	// Add a JLabel with text and set its foreground color
+ 		chatHeadLabel = new JLabel("Chat");
+ 		chatHeadLabel.setForeground(Color.BLACK);
+ 		chatHeadLabel.setFont(new Font("Default", Font.CENTER_BASELINE, 22));
+     	topPanel.add(chatHeadLabel);// Add the JLabel to the topPanel
+
         // Chat Area
         JTextArea chatArea = new JTextArea();
-        chatArea.setEditable(false);
         JScrollPane chatScrollPane = new JScrollPane(chatArea);
 
         // Chat Input
         JTextField chatInput = new JTextField();
         JButton sendButton = new JButton("Send");
         JPanel inputPanel = new JPanel(new BorderLayout());
-        sendButton.setFocusable(false);//removes the rectangle from around the send button
+        sendButton.setFocusable(false);
         inputPanel.add(chatInput, BorderLayout.CENTER);
         inputPanel.add(sendButton, BorderLayout.EAST);
 
         // Add components to the panel
+     	add(topPanel, BorderLayout.NORTH);
         add(chatScrollPane, BorderLayout.CENTER);
         add(inputPanel, BorderLayout.SOUTH);
     }
